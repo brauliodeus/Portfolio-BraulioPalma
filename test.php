ad30570bed1f0c70836b952f317d730e013357c9
Braulio Palma <braulio.palma05@gmail.com>
2026-05-25 21:29:50 -0400

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'includes/db.php';
-$data = json_decode('{"id": 1, "is_locked": 0}');
-if (!empty($data->id) && isset($data->is_locked)) {
-    echo "PATCH validation passed.\n";
-} else {
-    echo "PATCH validation failed!\n";
-}
